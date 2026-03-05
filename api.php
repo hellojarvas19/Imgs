b569d8f72724a180b84eb03f9bb1ee93cec3f343
hellojarvas19 <hellojarvas19@users.noreply.github.com>
2026-03-05 07:44:46 +0000

    Api

--- api.php
+++ api.php
@@ -4,7 +4,7 @@ error_reporting(0);
 ini_set('display_errors', 0);
 
 function json_response($price, $response) {
-    echo json_encode(['Price' => $price, 'Response' => $response]);
+    echo json_encode(['Price' => $price, 'Response' => $response], JSON_UNESCAPED_SLASHES);
     exit;
 }
 
